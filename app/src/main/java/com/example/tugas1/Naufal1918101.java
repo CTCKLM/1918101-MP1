836535be4ce677ab4396d02810f3b5613ab66816
CTCKLM <kasih.asyam@gmail.com>
2021-10-14 07:53:41 +0700

    CTCKLM

---
+++ app/src/main/java/com/example/tugas1/Naufal1918101.java
@@ -0,0 +1,15 @@
+package com.example.tugas1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Naufal1918101 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_naufal1918101);
+    }
+
+}
